08117a5ccdaeba2a63779a92479dbb9d6e1dd3cd
berbu25 <sergio.berbesi@udea.edu.co>
2023-11-02 14:44:25 -0500

    Creación del esqueleto del programa.

---
+++ parcial_2/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+// Clase para representar una casilla en el tablero
+class Casilla {
+public:
+    enum class Estado { vacia, blanca, negra };
+    Estado estado;
+
+    Casilla() : estado(Estado::vacia) {}
+};
+
+// Clase para representar un jugador
+class Jugador {
+public:
+    enum class Color { blanco, negro };
+    Color color;
+
+    Jugador(Color c) : color(c) {}
+
+    Color get_color() const {
+        return color;
+    }
+};
+
+// Clase para representar el tablero
+class Tablero {
+public:
+    static const int tamaño = 8;
+    vector<vector<Casilla>> tablero;
+
+    Tablero() {
+        tablero.resize(tamaño, std::vector<Casilla>(tamaño));
+    }
+
+    // Inicializa el tablero con las fichas iniciales
+    void inicializar_tablero() {
+        // Implementa la colocación inicial de fichas negras y blancas
+    }
+
+    // Imprime el tablero en la consola
+    void mostrar_tablero() {
+        // Implementa la visualización del tablero
+    }
+
+    // Implementa la lógica para verificar si una jugada es válida y realizarla
+    bool hacer_jugada(int fila, int columna, Jugador jugador) {
+        // Implementa la lógica de las reglas del juego
+        return false; // Devuelve verdadero si la jugada es válida
+    }
+
+    // Implementa la lógica para determinar si un jugador puede realizar una jugada
+    bool jugador_puede_jugar(Jugador jugador) {
+        // Implementa la lógica para verificar si un jugador puede jugar
+        return false;
+    }
+
+    // Implementa la lógica para contar las fichas de cada jugador y determinar el ganador
+    void determinar_ganador(Jugador& jugadorBlanco, Jugador& jugadorNegro) {
+        // Implementa la lógica para contar las fichas y determinar el ganador
+    }
+};
+
+// Clase para administrar el juego
+class Juego {
+public:
+    Tablero tablero;
+    Jugador jugadorBlanco;
+    Jugador jugadorNegro;
+
+    Juego() : jugadorBlanco(Jugador(Jugador::Color::blanco)), jugadorNegro(Jugador(Jugador::Color::negro)) {
+        tablero.inicializar_tablero();
+    }
+
+    // Inicia y administra una partida
+    void jugar_partida() {
+        // Implementa el bucle del juego, maneja los turnos y determina el ganador
+    }
+
+    // Guarda los resultados de la partida en un archivo
+    void guardar_resultados() {
+        // Implementa la lógica para guardar los resultados en un archivo de texto
+    }
+};
+
+int main() {
+    Juego juego;
+    char continuar;
+
+    do {
+        juego.jugar_partida();
+        juego.guardar_resultados();
+        cout << "¿Desea jugar otra partida? (S/N): ";
+        cin >> continuar;
+    } while (continuar == 'S' || continuar == 's');
+
+    return 0;
+}
